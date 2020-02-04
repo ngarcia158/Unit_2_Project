a05470f757ddea24281de55fba95d3027db3f183
Nicholas Garcia <ngarcia158@west-mec.org>
2020-02-04 14:53:36 -0700

    Finish case 2 comments

--- case2/hg_report.js
+++ case2/hg_report.js
@@ -11,9 +11,11 @@
    Filename: hg_report.js
 	
 */
-var gameReport = "<h1>" + itemTitle + "</h1 \
-            <h2>By:" + itemManufacturer + "</h2 \
-            <img src='hg_" + itemID + ".png' alt='id' id='gameImg' /> \
+//Makes the cariable gameReport and stores HTML code into it with the variables from the other javascript that store the title
+//and platform we are using 
+var gameReport = "<h1>" + itemTitle + "</h1> \
+            <h2>By:" + itemManufacturer + "</h2> \
+            <img src='hg_" +itemID+ ".png' alt='id' id='gameImg' /> \
             <table> \
             <tr><th>Product Id</th><td>"+itemID+"</td></tr> \
             <tr><th>List Price</th><td>"+itemPrice+"</td></tr> \
@@ -21,33 +23,45 @@ var gameReport = "<h1>" + itemTitle + "</h1 \
             <tr><th>ESRB Rating</th><td>"+itemESRB+"</td></tr> \
             <tr><th>Condition</th><td>"+itemCondition+"</td></tr> \
             <tr><th>Release</th><td>"+itemRelease+"</td></tr> \
-            </table> \
-            "+itemSummary;
+            </table> " +itemSummary;
 
+//makes the ratings sum variable equal 0
 var ratingsSum = 0;
+//makes the ratingsCount equal to the length of the ratings array
 var ratingsCount = ratings.length;
+//makes it so it goes through the loop and adds the value in the ratings and adds it to the ratingsSUm
 for(var i = 0; i<ratings.length; i++){
-   ratings[i] += ratingsSum;
+   ratingsSum += ratings[i];
 }
-
+//divides the raitngs sum by the total number of ratings to get the ratings Aerage
 var ratingsAvg = ratingsSum/ratingsCount;
-var ratingReport = "<h1>Customer Reviews</h1> \
-               <h2>"+ratingsAvg+"out of 5 stars ("+ratingsCount+" reviews)</h2>";
 
-for(var i = 0; ratings[0,1,2]; i++){
+//Makes the ratingReport equal to the html code of ratings review and adds the average and count into the code
+var ratingReport = "<h1>Customer Reviews</h1> \
+               <h2>"+ratingsAvg+" out of 5 stars ("+ratingsCount+" reviews)</h2>";
+
+//makes a for loop to different ratings be places on the page
+for(var i = 0; i<3; i++){
    ratingReport+= "<div class='review'> \
-               <h1>"+ratingTitles+"</h1> \
+               <h1>"+ratingTitles[i]+"</h1> \
                <table> \
-               <tr><th>By</th><td>"+ratingAuthors+"</td></tr> \
-               <tr><th>Review Date</th><td>"+ratingDates+"</td></tr> \
-               <tr><th>Rating</th>/tr>";
-               for(var i = 0; ratings[i]; i++){
-                  ratingReport += "<img src='hg_star.png'/>";
+               <tr><th>By</th><td>"+ratingAuthors[i]+"</td></tr> \
+               <tr><th>Review Date</th><td>"+ratingDates[i]+"</td></tr> \
+               <tr><th>Rating</th><td>";
+               //places number of starts the rating is 
+               for(var p = 0; p<ratings[i]; p++){
+                  ratingReport += "<img src='hg_star.png' />";
                }
-   ratingReport+="</td></tr></table>\
-      "+ratingSummaries
+   //adds to the rating report variable and closes the table and adds the summary and fianlly finishes the whole rating 
+   ratingReport+="</td></tr></table> \
+   " +ratingSummaries[i]
+    +"</div>";
 
 }
-document.getElementsByTagName("aside").innerHTML = ratingReport;
-document.getElementsByTagName("article").innerHTML = gameReport;
+//places the gameReport into article
+document.getElementsByTagName("article")[0].innerHTML = gameReport;
+//places the ratingReport into aside
+document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
+
+
 
